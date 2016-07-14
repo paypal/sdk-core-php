c43432e29b7c28d8cf10e162bfcd3a5f8edaea92
Tom Kay <oridan82@gmail.com>
2016-07-14 17:22:12 +0100

    fix character encoding issue

--- lib/PayPal/Auth/IPPThirdPartyAuthorization.php
+++ lib/PayPal/Auth/IPPThirdPartyAuthorization.php
@@ -5,7 +5,7 @@ namespace PayPal\Auth;
  *
  * Marker interface for implementations of third party authorization.
  * Third-party authorization allows you to make an API call on some
- * other account�s behalf
+ * other account's behalf
  */
 interface IPPThirdPartyAuthorization
 {
